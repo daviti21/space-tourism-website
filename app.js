8a61deae64e55cae902d118802d9321eb474f085
Davit Modebadze <dmodebadze63@gmail.com>
2026-03-07 15:39:10 +0400

    add image folder

--- app.js
+++ app.js
@@ -1,22 +1,27 @@
 const body = document.querySelector("body");
 
- const backgrounds = { 
-    "index.html": 
-    { desktop: "background-home-desktop.jpg",
-         tablet: "background-home-tablet.jpg",
-          mobile: "background-home-mobile.jpg" },
-  "crew.html":
-   { desktop: "background-crew-desktop.jpg", 
-    tablet: "background-crew-tablet.jpg",
-     mobile: "background-crew-mobile.jpg" }, 
-   "destination.html": 
-   { desktop: "background-destination-desktop.jpg",
-     tablet: "background-destination-tablet.jpg",
-      mobile: "background-destination-mobile.jpg" },
-    "technology.html": 
-    { desktop: "background-technology-desktop.jpg", 
-        tablet: "background-technology-tablet.jpg",
-         mobile: "background-technology-mobile.jpg" } };
+const backgrounds = { 
+    "index.html": { 
+        desktop: "images/background-home-desktop.jpg",
+        tablet: "images/background-home-tablet.jpg",
+        mobile: "images/background-home-mobile.jpg" 
+    },
+    "crew.html": { 
+        desktop: "images/background-crew-desktop.jpg", 
+        tablet: "images/background-crew-tablet.jpg",
+        mobile: "images/background-crew-mobile.jpg" 
+    }, 
+    "destination.html": { 
+        desktop: "images/background-destination-desktop.jpg",
+        tablet: "images/background-destination-tablet.jpg",
+        mobile: "images/background-destination-mobile.jpg" 
+    },
+    "technology.html": { 
+        desktop: "images/background-technology-desktop.jpg", 
+        tablet: "images/background-technology-tablet.jpg",
+        mobile: "images/background-technology-mobile.jpg" 
+    } 
+};
 
  const currentPage = window.location.pathname.split("/").pop() || "index.html";;
 
